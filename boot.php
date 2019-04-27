8936caa2e8f4362b81bb1806eb0a3aaaf37762ed
OpenTHC <code@openthc.org>
2019-04-27 08:25:07 +0000

    first!

---
+++ boot.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * OpenTHC QA Application Bootstrap
+ */
+
+define('APP_NAME', 'OpenTHC | QA');
+define('APP_SITE', 'https://qa.openthc.org');
+define('APP_ROOT', __DIR__);
+define('APP_SALT', sha1(APP_NAME . APP_SITE . APP_ROOT));
+define('APP_BUILD', '420.18.286');
+
+openlog('openthc-qa', LOG_ODELAY|LOG_PID, LOG_LOCAL0);
+
+error_reporting(E_ALL & ~ E_NOTICE);
+
+require_once(APP_ROOT . '/vendor/autoload.php');
+require_once(APP_ROOT . '/lib/App.php');
---
+++ webroot/front.php
@@ -0,0 +1,175 @@
+<?php
+/**
+ * Front Controller for qa.openthc.org
+ */
+
+require_once(dirname(dirname(__FILE__)) . '/boot.php');
+
+\App::db();
+
+
+// Slim Application
+//$cfg = array();
+$cfg = array('debug' => true);
+$app = new \OpenTHC\App($cfg);
+
+// Pub Home
+$app->get('/', function($REQ, $RES, $ARG) {
+	$data = array(
+		'Page' => array('title' => 'QA Laboratory Portal - OpenTHC'),
+	);
+	return $this->view->render($RES, 'page/home-pub.html', $data);
+})->add('App\Middleware\Menu');
+
+
+// App Home
+$app->get('/home', 'App\Controller\Home')
+->add('App\Middleware\Auth')
+->add('App\Middleware\Menu')
+->add('App\Middleware\Session');
+
+$app->get('/intent', 'App\Controller\Home:intent')->add('App\Middleware\Session');
+
+
+// Authentication
+$app->group('/auth', function() {
+
+	// Unique Auth/Open for this site
+	$this->get('/open', 'App\Controller\Auth\Open');
+	$this->post('/open', 'App\Controller\Auth\Open');
+
+	//$this->map(['GET', 'POST'], '/connect', 'OpenTHC\Controller\Auth\Connect');
+	$this->map(['GET', 'POST'], '/connect', 'App\Controller\Auth\Connect');
+
+	// oAuth Stuff
+	$this->get('/back', 'App\Controller\Auth\oAuth2\Back');
+
+	//$this->get('/oauth/open', 'App\Controller\Auth\oAuth2\Open');
+
+	$this->get('/fail', 'OpenTHC\Controller\Auth\Fail');
+	$this->get('/ping', 'OpenTHC\Controller\Auth\Ping');
+	$this->get('/shut', 'OpenTHC\Controller\Auth\Shut');
+
+})
+->add('App\Middleware\Menu')
+->add('App\Middleware\Session');
+
+
+// Sample Submit
+$app->group('/api', function() {
+
+	$this->get('', function($REQ, $RES) {
+		$data = array('Page' => array('title' => 'API'));
+		return $this->view->render($RES, 'page/home-api.html', $data);
+	});
+
+	$this->group('/v1', function() {
+
+		// Return List of Samples
+		$this->get('/qa', function($REQ, $RES, $ARG) {
+			return require_once(APP_ROOT . '/api/qa/search.php');
+		})->add('App\Middleware\Auth');
+
+		//$this->post('/qa', function($REQ, $RES, $ARG) {
+		//	die('Create QA Sample');
+		//})->add($MWA);
+
+		//$this->get('/qa/sample', function($REQ, $RES, $ARG) {
+		//	die('List QA Samples');
+		//})->add($MWA);
+		$this->get('/result/{guid}.pdf', function($REQ, $RES, $ARG) {
+			$ARG['code'] = preg_replace('/\s+/', '', $ARG['guid']);
+			return require_once(APP_ROOT . '/api/qa/result.pdf.php');
+		});
+
+		// Create a Sample
+		//$this->post('/qa/sample', function($REQ, $RES, $ARG) {
+		//	require_once(APP_ROOT . '/api/qa/sample-create.php');
+		//})->add('Middleware_Auth');
+
+		// Select Specific QA Sample
+		$this->get('/qa/{code}', function($REQ, $RES, $ARG) {
+			$ARG['code'] = preg_replace('/\s+/', '', $ARG['code']);
+			return require_once(APP_ROOT . '/api/qa/sample.php');
+		});
+
+		// Select QA Sample+Result
+		$this->get('/qa/{code}/result', function($REQ, $RES, $ARG) {
+			require_once(APP_ROOT . '/api/qa/sample.php');
+		});
+
+		// Update QA Result
+		//$this->post('/qa/{code}/result', function($REQ, $RES, $ARG) {
+		//	require_once(APP_ROOT . '/api/qa/result.php');
+		//})->add('Middleware_Auth');
+
+	})
+	->add('MiddlewareJSON')
+	->add('MiddlewareLogHTTP')
+	;
+
+});
+
+
+// Transfer Group
+$app->group('/transfer', function() {
+
+	$this->get('', 'App\Controller\Transfer');
+
+	$this->get('/accept', 'App\Controller\Transfer\Accept');
+	$this->post('/accept', 'App\Controller\Transfer\Accept');
+
+	$this->get('/{guid}', 'App\Controller\Transfer\View');
+
+	//$app->get('/transfer/import/{guid}', 'App\Controller\Transfer\Import')
+	//	->add('Middleware_Auth')
+	//	;
+
+})
+->add('App\Middleware\Auth')
+->add('App\Middleware\Menu')
+->add('App\Middleware\Session');
+
+
+// Sample Group
+$app->group('/sample', 'App\Module\Sample')
+->add('App\Middleware\Auth')
+->add('App\Middleware\Menu')
+->add('App\Middleware\Session');
+
+
+// Result Group
+$app->group('/result', 'App\Module\Result')
+->add('App\Middleware\Auth')
+->add('App\Middleware\Menu')
+->add('App\Middleware\Session');
+
+
+// No Session Here
+$app->get('/share', 'App\Controller\Share')
+->add('App\Middleware\Auth')
+->add('App\Middleware\Menu')
+->add('App\Middleware\Session');
+
+$app->get('/share/{guid}', 'App\Controller\Result\Share')
+->add('App\Middleware\Menu')
+->add('App\Middleware\Session');
+
+// Sync
+$app->get('/sync', 'App\Controller\Sync')
+->add('App\Middleware\Menu')
+->add('App\Middleware\Session');
+
+$app->any('/sync/exec', 'App\Controller\Sync:exec')
+->add('App\Middleware\Session');
+
+
+// No Session Here
+$app->get('/about', function($REQ, $RES, $ARG) {
+	$data = array('Page' => array('title' => 'About'));
+	return $this->view->render($RES, 'page/about.html', $data);
+})->add('App\Middleware\Menu');
+
+
+// Execute Slim
+$res = $app->run();
