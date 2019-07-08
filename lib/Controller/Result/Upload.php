d0f4772a7c683e0b31f83def86380ed6be4ebd6e
djbusby <code@edoceo.com>
2019-07-08 16:46:38 +0000

    Refine upload

--- lib/Controller/Result/Upload.php
+++ lib/Controller/Result/Upload.php
@@ -12,6 +12,34 @@ class Upload extends \OpenTHC\Controller\Base
 		$data = array();
 		$data['Page'] = array('title' => 'Result :: COA :: Upload');
 
-		return $this->_container->view->render($RES, 'page/result/upload.html', $data);
+		switch ($_SERVER['REQUEST_METHOD']) {
+		case 'GET':
+			return $this->_container->view->render($RES, 'page/result/upload.html', $data);
+		case 'POST':
+
+			// var_dump($_POST);
+			// var_dump($_FILES);
+
+			if (1 == count($_FILES)) {
+				if (0 == $_FILES['file']['error']) {
+
+					$import_queue_path = sprintf('%s/var/import/%s', APP_ROOT, $_SESSION['License']['id']);
+					$import_queue_file = sprintf('%s/%s', $import_queue_path, urlencode($_FILES['file']['name']));
+
+					if (!is_dir($import_queue_path)) {
+						mkdir($import_queue_path, 0755, true);
+					}
+
+					move_uploaded_file($_FILES['file']['tmp_name'], $import_queue_file);
+
+				}
+			}
+
+			return $RES->withJSON(array(
+				'status' => 'success',
+			));
+
+			break;
+		}
 	}
 }
