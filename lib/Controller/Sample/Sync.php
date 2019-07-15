8f8dc3cc6dc01ea6139e2a6444572ebb5c19b8ed
djbusby <code@edoceo.com>
2019-07-15 20:14:43 +0000

    Improve Sample Sync

--- lib/Controller/Sample/Sync.php
+++ lib/Controller/Sample/Sync.php
@@ -15,6 +15,7 @@ class Sync extends \OpenTHC\Controller\Base
 	 */
 	function __invoke($REQ, $RES, $ARG)
 	{
+		$_GET['a'] = 'force';
 		if (!empty($_GET['a'])) {
 			if ('force' == $_GET['a']) {
 				unset($_SESSION['sync-sample-time']);
@@ -23,9 +24,9 @@ class Sync extends \OpenTHC\Controller\Base
 		if (!empty($_SESSION['sync-sample-time'])) {
 			$span = $_SERVER['REQUEST_TIME'] - $_SESSION['sync-sample-time'];
 			if ($span <= 900) {
-				//return $RES->withRedirect('/sample');
 				return $RES->withJSON(array(
 					'status' => 'success',
+					'detail' => 'Cached',
 					'result' => 0,
 				));
 			}
@@ -34,6 +35,9 @@ class Sync extends \OpenTHC\Controller\Base
 		$_SESSION['sync-sample-time'] = $_SERVER['REQUEST_TIME'];
 
 		session_write_close();
+
+		$c_ins = 0;
+		$c_upd = 0;
 
 		// Only want to get the QA Sample Lots -- so maybe, only Lots when we are a Laboratory?
 		$cre = new \OpenTHC\RCE($_SESSION['pipe-token']);
@@ -52,8 +56,8 @@ class Sync extends \OpenTHC\Controller\Base
 
 			$Lot = $rec['_source'];
 
-			$chk = SQL::fetch_one('SELECT id FROM lab_sample WHERE license_id = ? AND id = ?', array($_SESSION['License']['id'], $rec['guid']));
-			if (empty($chk)) {
+			$chk = SQL::fetch_row('SELECT id, meta FROM lab_sample WHERE license_id = ? AND id = ?', array($_SESSION['License']['id'], $rec['guid']));
+			if (empty($chk['id'])) {
 
 				$x = $cre->get('/config/product/' . $Lot['global_inventory_type_id']);
 				$Product = $x['result'];
@@ -77,6 +81,20 @@ class Sync extends \OpenTHC\Controller\Base
 				);
 
 				SQL::insert('lab_sample', $add);
+				$c_ins++;
+
+			} else {
+
+
+				// Update the Meta
+				$m = json_decode($chk['meta'], true);
+				$m['Lot'] = $Lot;
+
+				SQL::query('UPDATE lab_sample SET meta = :m0 WHERE id = :pk', array(
+					':pk' => $chk['id'],
+					':m0' => \json_encode($m)
+				));
+				$c_upd++;
 
 			}
 		}
@@ -85,6 +103,7 @@ class Sync extends \OpenTHC\Controller\Base
 
 		return $RES->withJSON(array(
 			'status' => 'success',
+			'detail' => sprintf('%d Insert, %d Update', $c_ins, $c_upd),
 		));
 
 	}
