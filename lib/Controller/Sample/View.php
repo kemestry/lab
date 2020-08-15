1aa7f7f2da8c75a859b3eccec92d81b9cc63deb0
djbusby <code@edoceo.com>
2020-08-15 00:13:18 +0000

    Improve Sample Edit

--- lib/Controller/Sample/View.php
+++ lib/Controller/Sample/View.php
@@ -19,19 +19,6 @@ class View extends \App\Controller\Base
 			_exit_text('Invalid Request', 400);
 		}
 
-		switch ($_POST['a']) {
-			case 'drop':
-				// need to return the $RES object from these methods to do anything
-				return $this->_dropSample($RES, $ARG);
-			break;
-			case 'void':
-				$this->_voidSample($RES, $ARG);
-			break;
-			case 'done':
-				return $this->_finishSample($RES, $ARG);
-			break;
-		}
-
 		$dbc = $this->_container->DB;
 
 		// $this->cre = new \OpenTHC\CRE($_SESSION['pipe-token']);
@@ -40,11 +27,28 @@ class View extends \App\Controller\Base
 		if (empty($Lab_Sample['id'])) {
 			_exit_text('Invalid Lab Sample [CSV#032]', 400);
 		}
+
+		switch ($_POST['a']) {
+			case 'done':
+				return $this->_finishSample($RES, $ARG, $Lab_Sample);
+			break;
+			case 'drop':
+				// need to return the $RES object from these methods to do anything
+				return $this->_dropSample($RES, $ARG, $Lab_Sample);
+			break;
+			case 'save':
+				$this->_saveSample($RES, $Lab_Sample);
+			break;
+			case 'void':
+				return $this->_voidSample($RES, $ARG, $Lab_Sample);
+			break;
+		}
+
 		// var_dump($Lab_Sample);
 
 		$Product = $dbc->fetchRow('SELECT * FROM product WHERE id = ?', [ $Lab_Sample['product_id'] ]);
 		$ProductType = $dbc->fetchRow('SELECT * FROM product_type WHERE id = ?', [ $Product['product_type_id'] ]);
-		$Strain = $dbc->fetchRow('SELECT * FROM strain WHERE id = ?', [ $Lab_Sample['strain_id'] ]);
+		$Variety = $dbc->fetchRow('SELECT * FROM strain WHERE id = ?', [ $Lab_Sample['strain_id'] ]);
 
 		$LSm = json_decode($Lab_Sample['meta'], true);
 
@@ -60,22 +64,27 @@ class View extends \App\Controller\Base
 		//$L_Lab = $res['result'];
 
 		// Find Owner License
-		// $res = $this->cre->get('/config/license/' . $LSm['Lot']['global_created_by_mme_id']);
-		// $L_Own = new \OpenTHC\License($dbc);
-		// $L_Own->loadBy('id', $LSm['Lot']['global_created_by_mme_id']);
+		$dbc_main = $this->_container->DBC_Main;
+		$L_Source = new \OpenTHC\License($dbc_main, $Lab_Sample['license_id_source']);
 
 		$data = $data = $this->loadSiteData([
 			'Page' => array('title' => 'Sample :: View'),
 			// 'Lab_Sample' => $Lab_Sample->toArray(), // @deprecated
 			// 'Sample' => $LSm['Lot'], // @deprecated
-			'Lot' => $Lab_Sample->toArray(),
+			'Sample' => $Lab_Sample->toArray(),
 			'Product' => $Product,
 			'ProductType' => $ProductType,
-			'Strain' => $Strain,
+			'Variety' => $Variety,
 			// 'Result' => $LSm['Result'],
-			// 'License_Source' => $L_Own->toArray(),
+			'License_Source' => $L_Source->toArray(),
 		]);
 		$data['Sample']['id'] = $ARG['id'];
+
+		// Nicely Formatted ID
+		$data['Sample']['id_nice'] = $ARG['id'];
+		if (!empty($data['Sample']['guid'])) {
+			$data['Sample']['id_nice'] = $data['Sample']['guid'];
+		}
 		if (empty($data['Product']['uom'])) {
 			$data['Product']['uom'] = 'g';
 		}
@@ -84,6 +93,8 @@ class View extends \App\Controller\Base
 		if ('flower' == $data['Product']['intermediate_type']) {
 			$data['Sample']['medically_compliant'] = true;
 		}
+
+		$data['lab_result_list'] = $dbc->fetchAll('SELECT id, name FROM lab_result WHERE lab_sample_id = :ls0', [ ':ls0' => $Lab_Sample['id'] ]);
 
 		return $this->_container->view->render($RES, 'page/sample/view.html', $data);
 
@@ -116,9 +127,10 @@ class View extends \App\Controller\Base
 
 		// $res = $this->cre->get('/lot?source=true');
 		// $res = $this->cre->delete('/lot/' . $ARG['id']);
+		$dbc_user = $this->_container->DBC_User;
 
 		$sql = 'SELECT * from lab_sample where id = :pk';
-		$res = $dbc->fetchAll($sql, [
+		$res = $dbc_user->fetchAll($sql, [
 			':pk' => $ARG['id'],
 		]);
 
@@ -129,8 +141,48 @@ class View extends \App\Controller\Base
 			':s1' => \App\Lab_Sample::STAT_VOID,
 			':f1' => \App\Lab_Sample::FLAG_DEAD,
 		);
-		$res = $dbc->query($sql, $arg);
+		$res = $dbc_user->query($sql, $arg);
 
+		return $RES->withRedirect('/sample');
+	}
+
+	function _saveSample($RES, $Lab_Sample)
+	{
+		$dbc = $this->_container->DBC_User;
+
+		if (!empty($_POST['product-name'])) {
+			$arg = [];
+			$arg[':p0'] = trim($_POST['product-name']);
+			$PR = $dbc->fetchRow('SELECT id FROM product WHERE name = :p0', $arg);
+			if (!empty($PR['id'])) {
+				$Lab_Sample['product_id'] = $PR['id'];
+			}
+		}
+
+		if (!empty($_POST['variety-id'])) {
+			$arg = [];
+			$arg[':v0'] = $_POST['variety-id'];
+			$VT = $dbc->fetchRow('SELECT id FROM strain WHERE id = :v0', $arg);
+			if (empty($VT['id'])) {
+				$VT = [
+					'id' => $_POST['variety-id'],
+					'license_id' => $_SESSION['License']['id'],
+					'guid' => $_POST['variety-id'],
+					'name' => $_POST['variety-name'],
+				];
+				$dbc->insert('strain', $VT);
+			}
+			if (!empty($VT['id'])) {
+				$Lab_Sample['strain_id'] = $VT['id'];
+			}
+		}
+
+		if (!empty($_POST['sample-qty'])) {
+			$Lab_Sample['qty'] = floatval($_POST['sample-qty']);
+		}
+
+		$Lab_Sample['license_id_source'] = $_POST['license-id-source'];
+		$Lab_Sample->save();
 		return $RES->withRedirect('/sample');
 	}
 
