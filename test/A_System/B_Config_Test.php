efd81e53f99ca61b1f6507348c2fc80a8253b7ea
openthc <code@openthc.com>
2021-02-02 18:23:47 +0000

    update test tools

--- test/A_System/B_Config_Test.php
+++ test/A_System/B_Config_Test.php
@@ -7,4 +7,22 @@ namespace Test\System;
 
 class B_Config_Test extends \Test\Base
 {
+	function test_oauth()
+	{
+		$cfg = \OpenTHC\Config::get('oauth');
+
+		$key_list = [
+			'hostname',
+			'clientId',
+			'clientSecret',
+			'urlAuthorize',
+			'urlAccessToken',
+			'urlResourceOwnerDetails',
+		];
+
+		foreach ($key_list as $k) {
+			$this->assertArrayHasKey($cfg, $k, "oAuth '$k' not set");
+		}
+
+	}
 }
